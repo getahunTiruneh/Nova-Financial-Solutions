aecc472a37981f7de622e9c292d75bbec9654a26
getahunTiruneh <gechtiru@gmail.com>
2024-08-28 16:00:36 +0300

    modify text analyzer scripts

--- scripts/text_analysis.py
+++ scripts/text_analysis.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import nltk
+from nltk.corpus import stopwords
 from textblob import TextBlob
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
@@ -48,6 +51,16 @@ class TextAnalysis:
         print("Article Counts per Publisher:")
         return publisher_counts
     
+    def plot_article_per_publisher(self):
+        publisher_counts = self.article_per_publisher().nlargest(5, 'Article Count')
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x='Publisher', y='Article Count', data=publisher_counts)
+        plt.xticks(rotation=90)
+        plt.xlabel('Publisher')
+        plt.ylabel('Number of Articles')
+        plt.title('Number of Articles per Publisher')
+        plt.show()
+    
     def publication_dates_analysis(self):
         self.data['date']=pd.to_datetime(self.data['date'],format='ISO8601')
         self.data['year'] = self.data['date'].dt.year
@@ -60,53 +73,78 @@ class TextAnalysis:
         plt.ylabel('Number of Articles')
         plt.title('Number of Articles per Day of the Month')
         plt.show()
-    
-        # Text Analysis
-    def sentiment_analysis(self):
-        self.data['sentiment'] = self.data['headline'].apply(lambda x: TextBlob(x).sentiment.polarity)
-        self.data['sentiment']=['positive' if x>0 else 'Negative' if x<0 else 'Neutral' for x in self.data['sentiment']]
-        self.data['sentiment'].value_counts().plot(kind='bar',figsize=(12,6))
-        plt.xlabel('Sentiment')
-        plt.ylabel('Number of Articles')
-        plt.title('Sentiment Analysis')
-        plt.show()
-    
-    def word_frequency(self):
-        all_text = ' '.join(self.data['headline'])
-        word_freq = pd.Series(all_text.split()).value_counts()
-        word_freq[:20].plot(kind='bar', figsize=(12, 6))
-        plt.xlabel('Words')
-        plt.ylabel('Frequency')
-        plt.title('Word Frequency Distribution')
-        plt.show()
-        
     # Time Series Analysis
-    def publication_frequency_over_time(self):
+    def publication_frequency_over_time(self): # Group the data by year and month, and count the number of articles for each group
+        self.publication_dates_analysis()
         articles_per_month = self.data.groupby(['year', 'month']).size()
-        articles_per_month.plot()
-        plt.xlabel('Month')
-        plt.ylabel('Number of Articles')
-        plt.title('Articles per Month')
-        plt.show()
-     
-     # Time Series Analysis
-    def publication_frequency_over_time(self):
-        articles_per_month = self.data.groupby(['year', 'month']).size()
-        articles_per_month.plot()
+        articles_per_month.plot() # Plot the time series data
         plt.xlabel('Month')
         plt.ylabel('Number of Articles')
         plt.title('Articles per Month')
         plt.show()
        
     def unique_domains_from_emails(self):
-        if self.data['publisher'].str.contains('@').any():
-            self.data['domain'] = self.data['publisher'].apply(lambda x: x.split('@')[-1] if '@' in x else None)
+        if self.data['publisher'].str.contains('@').any(): 
+            self.data['domain'] = self.data['publisher'].apply(lambda x: x.split('@')[-1] if '@' in x else None) # Extract the domain from the email addresses
             return self.data['domain'].value_counts()
         
         
+        
+        # Text Analysis
+    def text_preprocess(self):
+        self.drop_unnamed_column()
+        # Convert to lowercase and remove non-alphabetic characters
+        self.data['cleand_headline']=self.data['headline'].str.lower().str.replace(r'[^a-zA-Z\s]', '', regex=True) 
+        # Remove leading and trailing whitespace
+        self.data['cleand_headline']=self.data['cleand_headline'].str.strip() 
+        # remove stop words
+        stop_words = set(stopwords.words('english'))
+        self.data['cleand_headline'] = self.data['cleand_headline'].apply(lambda x: ' '.join([word for word in x.split() if word not in stop_words]))
+        return self.data
+    
+    def get_sentiment(self):
+        # First preprocess the text
+        self.text_preprocess()
+        # Calculate the sentiment polarity of each headline
+        self.data['polarity'] = self.data['cleand_headline'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        # Categorize the sentiment based on the polarity score
+        self.data['sentiment'] = self.data['polarity'].apply(lambda x: 'positive' if x > 0 else 'Negative' if x < 0 else 'Neutral')
+        return self.data
+    def plot_sentiment_distribution(self):
+        # First preprocess the text
+        self.text_preprocess()
+        
+        # Visualize the sentiment distribution
+        sentiment_counts = self.get_sentiment()['sentiment'].value_counts()
+        print(sentiment_counts)  # Print the counts for each sentiment category
+        plt.figure(figsize=(8, 6)) # Set the figure size
+        sns.barplot(x=sentiment_counts.index, y=sentiment_counts.values)  # Create a bar plot
+        plt.xticks(rotation=45)  # Rotate the x-axis labels for better readability
+        plt.xlabel('Sentiment')  # Set the x-axis label
+        plt.ylabel('Number of Articles')  # Set the y-axis label
+        plt.title('Sentiment Distribution')  # Set the title of the plot
+        plt.show()  # Display the plot
+    
+    def word_frequency(self):
+        # First preprocess the text
+        self.text_preprocess()
+        # Concatenate all headlines into a single string
+        all_text = ' '.join(self.data['cleand_headline']) 
+        # Count the frequency of each word
+        word_freq = pd.Series(all_text.split()).value_counts() 
+        # Plot the top 20 most frequent words
+        word_freq[:20].plot(kind='bar', figsize=(12, 6))
+        plt.xticks(rotation=45)
+        plt.xlabel('Words') 
+        plt.ylabel('Frequency')
+        plt.title('Word Frequency Distribution')
+        plt.show()        
+        
     def topic_modeling(self, n_topics=5):
+        # First preprocess the text
+        self.text_preprocess()
         vectorizer = CountVectorizer(stop_words='english')
-        X = vectorizer.fit_transform(self.data['headline'])
+        X = vectorizer.fit_transform(self.data['cleand_headline'])
         lda = LatentDirichletAllocation(n_components=n_topics, random_state=42)
         lda.fit(X)
     
